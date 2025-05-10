a3b35c56e73cb1be97c80abbb4f5de60f634efda
Davidt50796 <ugashudav@gmail.com>
2025-05-10 20:58:05 +0200

    staging: quick fixes on select options

--- Portal/src/api/images.ts
+++ Portal/src/api/images.ts
@@ -33,7 +33,7 @@ export async function addNewImage(imageObj: ImageObj) {
 }
 
 export async function updateImage(imageObj: updateImageObj) {
-    let body: any = imageObj
+    let body: updateImageObj | FormData = imageObj
     if (imageObj.image) {
         const formData = new FormData();
         formData.append('image', imageObj.image);
